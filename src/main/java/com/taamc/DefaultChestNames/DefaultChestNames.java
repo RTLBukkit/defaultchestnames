452bad237ca9dccc708aae81c945324146238f60
ryantheleach <rleach@rleach.id.au>
2020-08-24 17:40:19 +0930

    Update for 1.16

--- src/main/java/com/taamc/DefaultChestNames/DefaultChestNames.java
+++ src/main/java/com/taamc/DefaultChestNames/DefaultChestNames.java
@@ -1,56 +1,46 @@
 package com.taamc.DefaultChestNames;
 
-import java.util.Arrays;
-import java.util.List;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
+import com.google.common.base.Strings;
+import org.bukkit.ChatColor;
+import org.bukkit.Nameable;
+import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DefaultChestNames extends JavaPlugin implements Listener {
+
+	final ChatColor markerColor = ChatColor.DARK_GREEN;
 
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
-	final static List<Material> types = Arrays.asList(Material.CHEST, Material.DISPENSER, Material.TRAPPED_CHEST, Material.FURNACE,
-			Material.DROPPER, Material.ENCHANTMENT_TABLE, Material.HOPPER, Material.BREWING_STAND);
+	public void nameChest(BlockState nameableState, String name) {
+		final Nameable nameable = (Nameable) nameableState;
+		nameable.setCustomName(name);
+		nameableState.update();
+	}
+
+	public boolean isNamed(BlockState nameableState) {
+		final Nameable nameable = (Nameable) nameableState;
+		String customName = nameable.getCustomName();
+		if(customName == null) customName = "";
+		if(customName.contains(markerColor.toString())) return false;
+		return !Strings.isNullOrEmpty(customName);
+	}
 
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onChestPlace(BlockPlaceEvent e) {
-		if (e.isCancelled())
-			return;
-		final Block block = e.getBlock();
-		final Material mat = block.getType();
-		final boolean isNameable = types.contains(mat);
-		final Player player = e.getPlayer();
-		if (isNameable) {
-			final ItemStack item = e.getItemInHand();
-			final ItemMeta meta;
-			meta = item.getItemMeta();
-			if (!meta.hasDisplayName()) {
-				final String oldName = meta.getDisplayName();
-				meta.setDisplayName(e.getPlayer().getName());
-				item.setItemMeta(meta);
-				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-					public void run() {
-						final ItemStack item2 = player.getItemInHand();
-						final ItemMeta meta2 = item2.getItemMeta();
-						if(meta2!=null){
-							meta2.setDisplayName(oldName);
-							item2.setItemMeta(meta2);
-						}
-					}
-				}, 1);
-			}
+		final BlockState block = e.getBlock().getState();
+		final boolean isNameable = block instanceof Nameable;
 
+		if (isNameable) {
+			if(!isNamed(block)) {
+				nameChest(block, markerColor + e.getPlayer().getName());
+			}
 		}
 	}
 }
