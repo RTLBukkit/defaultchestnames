765700d7a0ebf5f38944264a38815f4c8e703d67
ryantheleach <rleach@rleach.id.au>
2022-01-07 06:59:58 +1030

    Fix: fixes bug where players would get their chests back on destroy with the original name.

--- src/main/java/com/taamc/DefaultChestNames/DefaultChestNames.java
+++ src/main/java/com/taamc/DefaultChestNames/DefaultChestNames.java
@@ -4,11 +4,19 @@ import com.google.common.base.Strings;
 import org.bukkit.ChatColor;
 import org.bukkit.Nameable;
 import org.bukkit.block.BlockState;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.event.block.BlockEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.naming.Name;
 
 public class DefaultChestNames extends JavaPlugin implements Listener {
 
@@ -32,15 +40,33 @@ public class DefaultChestNames extends JavaPlugin implements Listener {
 		return !Strings.isNullOrEmpty(customName);
 	}
 
-	@EventHandler(priority = EventPriority.HIGH)
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onChestPlace(BlockPlaceEvent e) {
 		final BlockState block = e.getBlock().getState();
-		final boolean isNameable = block instanceof Nameable;
-
-		if (isNameable) {
+		if (isNameable(e)) {
 			if(!isNamed(block)) {
 				nameChest(block, markerColor + e.getPlayer().getName());
 			}
+		}
+	}
+
+	boolean isNameable(BlockEvent e) {
+		final BlockState block = e.getBlock().getState();
+		return block instanceof Nameable;
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onChestDrop(BlockDropItemEvent e) {
+		if(!(e.getBlockState() instanceof Nameable)) return;
+		for (Item item : e.getItems()) {
+			final ItemStack stack = item.getItemStack();
+			final ItemMeta itemMeta = stack.getItemMeta();
+			final String name = itemMeta != null ? itemMeta.getDisplayName() : "";
+			if(!name.contains(markerColor.toString())) return;
+			assert itemMeta != null;
+			itemMeta.setDisplayName(null);
+			stack.setItemMeta(itemMeta);
+			item.setItemStack(stack);
 		}
 	}
 }
